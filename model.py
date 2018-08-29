ac720bb712489e31595915f827af7bfea13877b1
hitukensin <hitukensin2@gmail.com>
2018-08-29 19:59:20 +0900

    commit

--- model.py
+++ model.py
@@ -90,7 +90,7 @@ class Model():
 
         self.predictions = tf.nn.softmax(logits, name='predictions')
 
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(self.Y, [-1]))
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(self.Y, [-1]),logits=logits)
 
         mean,var = tf.nn.moments(logits, -1)
         self.loss = tf.reduce_mean(loss)
