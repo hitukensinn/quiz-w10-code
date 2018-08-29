e8711bbebe9964d7e6aaff3a7e7683f546c44432
hitukensin <hitukensin2@gmail.com>
2018-08-29 23:34:28 +0800

    commit

--- sample.py
+++ sample.py
@@ -26,10 +26,13 @@ vocabulary = read_data(FLAGS.text)
 data, count, dictionary, reverse_dictionary = utils.build_dataset(vocabulary, 5000)
 print("reverse_dictionary",len(reverse_dictionary))
 
-
+reverse_list = []
 for i in range(len(reverse_dictionary)):
+    logging.debug('^^^^^i^^^^^^',i)
     if isinstance(reverse_dictionary.get(str(i)),str):
+        logging.debug('^^^^^reverse_dictionary.get^^^^^^',reverse_dictionary.get(str(i)))
         reverse_list = [reverse_dictionary.get(str(i))]
+
 
 titles = ['江神子', '蝶恋花', '渔家傲']
 
